d302ca4ec936394407cace55b4401426a3b5c0b5
TheSilliestGeese <126857288+TheSilliestGeese@users.noreply.github.com>
2024-10-19 10:57:19 +0100

    ,

---
+++ README.md
@@ -0,0 +1,3 @@
+## My Muppets Show LOVE Engine Port
+Hello this is *unofficial* and not made by Disney and is a non-profit restoration project.
+# It is highly unfinished as of now.
--- main.lua
+++ main.lua
@@ -1,15 +1,21 @@
 dataset = require 'assets/scripts/dataset'
+roomy = require 'assets/scripts/roomy'
 gameDS = {}
+
 function love.load()
     print("loaded")
+    
     gameDS = dataset.parse('game')
     version = dataset.getFromData(gameDS,'ver')
     title = dataset.getFromData(gameDS,'title')
     devMode = dataset.getFromData(gameDS,'devMode')
+
     love.window.setTitle(title)
     love.mouse.setCursor(love.mouse.newCursor("assets/gfx/cursor.png", 0, 0))
-    -- devmode
+
+    
     if devMode == true then
+        -- devmode
         print("user is in devmode!")
         love.window.setTitle(love.window.getTitle()..' ('..version..') [DEVELOPER BUILD]')
     end
