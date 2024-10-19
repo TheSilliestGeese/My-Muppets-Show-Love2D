86e724ea2b538320cdaf8bea0e9afa61d95344e5
TheSilliestGeese <126857288+TheSilliestGeese@users.noreply.github.com>
2024-10-19 10:13:39 +0100

    m

--- assets/scripts/dataset.lua
+++ assets/scripts/dataset.lua
@@ -1,19 +1,25 @@
 json = require 'assets/scripts/json'
-
 local dataset = {}
-
 function dataset.parse(data)
-    return json.decode(love.filesystem.read('assets/data/'..data..'.json'))
+    if love.filesystem.read('assets/data/'..data..'.json') then
+        parsedData = json.decode(love.filesystem.read('assets/data/'..data..'.json'))
+        if parsedData then
+            print('parsed '..'assets/data/'..data..'.json as '..love.filesystem.read('assets/data/'..data..'.json'))
+            return parsedData
+        else
+            return {
+                error=true
+            }
+        end
+    end
 end
-
 function dataset.getFromData(set, var)
     if set and set[var] then
         response = set[var]
     else
-        response = false
-        print(var..' is not found in the DataSet provided, please check your json file for further assistance.')
+        response = "data not found"
+        print(var..' is not in the dataset')
     end
     return response
 end
-
 return dataset
--- main.lua
+++ main.lua
@@ -1,16 +1,16 @@
 dataset = require 'assets/scripts/dataset'
 gameDS = {}
-
 function love.load()
+    print("loaded")
     gameDS = dataset.parse('game')
     version = dataset.getFromData(gameDS,'ver')
     title = dataset.getFromData(gameDS,'title')
     devMode = dataset.getFromData(gameDS,'devMode')
-    love.window.setTitle(title..' ('..version..')')
-
+    love.window.setTitle(title)
     love.mouse.setCursor(love.mouse.newCursor("assets/gfx/cursor.png", 0, 0))
-
     -- devmode
-    love.window.setTitle(love.window.getTitle()..' [DEVELOPER BUILD]')
+    if devMode == true then
+        print("user is in devmode!")
+        love.window.setTitle(love.window.getTitle()..' ('..version..') [DEVELOPER BUILD]')
+    end
 end
-
